3bd0c6e7e12c446ca2c53482422aceb8233f1a66
afozbek <furkanozbek1995@gmail.com>
2019-09-15 14:32:25 +0300

    adding open classes with javascript

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,25 @@
 const menuButton = document.querySelector(".menu-btn");
 const hamburgerMenu = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = menuNav.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
 
 function toggleMenu() {
   if (!showMenu) {
     hamburgerMenu.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach(navItem => navItem.classList.add("open"));
 
     showMenu = true;
   } else {
     hamburgerMenu.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach(navItem => navItem.classList.remove("open"));
+
     showMenu = false;
   }
 }
